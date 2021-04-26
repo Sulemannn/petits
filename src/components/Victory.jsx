79346f428039c1b3f5e6dfc069e1a51df8d0f061
Suleman <suleman.khn10@gmail.com>
2021-04-26 17:18:05 +0200

    création d'un nouveau component (Vicotory.jsx )

---
+++ src/components/Victory.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Victory extends React.Component {
+
+    render () {
+
+        return (
+            <div>
+                renderVictory={this.props.victory}
+            </div>
+        )
+
+    }
+}
+
+export default Victory;
